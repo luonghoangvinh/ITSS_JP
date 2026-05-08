95ad63e515610de60297eccd255bffcff8856b5a
luonghoangvinh <lhvinh2003@gmail.com>
2026-05-08 19:45:01 +0700

    cập nhật sidebar cho giống thiết kế

--- itss_jp_vietTalk/src/components/Sidebar.tsx
+++ itss_jp_vietTalk/src/components/Sidebar.tsx
@@ -1,27 +1,47 @@
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
+import { BookOpen, GraduationCap, House, Rows3, Settings } from "lucide-react";
 export function Sidebar() {
     return (
             <div className="Sidebar">
                 <nav>
                     <Link id="Logo" to="/">VietTalk</Link>
                     <p id="LogoText">ベトナム語を学ぶ</p>
-                    <ul >
+                    <ul className="w-35">
                     <li>
-                        <Link to="/">ホーム</Link>
+                        <Link to="/">
+                        <span className="ml-1"><House size={20}/></span>
+                        <span className="ml-1.5">ホーム</span>
+                        </Link>
                     </li>
                     <li >
-                        <Link to="/Dictionary">辞書</Link>
+                        
+                        <Link to="/Dictionary">
+                            <span className="mt-1">{<BookOpen size={20}/>}</span>
+                            
+                            <span className="ml-1.5">辞書</span>
+                        </Link>
                     </li>
 
                     <li>
-                        <Link to="/Lessons">レッスン</Link>
+                        
+                        <Link to="/Lessons">
+                        <span className="mt-1">{<GraduationCap size={20}/>}</span>
+
+                            <span className="ml-1.5">レッスン</span>
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/Settings">設定</Link>
+                        <Link to="/Settings">
+                        <span className="mt-1">{<Settings size={20}/>}</span>
+                            <span className="ml-1.5">設定</span>
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/LevelAssessment">レベル判定</Link>
+                        <Link to="/LevelAssessment">
+                            <span className="mt-1">{<Rows3 size={20}/>}</span>
+                            <span className="ml-1.5">レベル判定</span>
+                        </Link>
                     </li>
                 </ul>
                 </nav>
--- itss_jp_vietTalk/src/utils/NavigationHandler.tsx
+++ itss_jp_vietTalk/src/utils/NavigationHandler.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-export const goToHome = () => {
+const goToHome = () => {
     const navigate = useNavigate();
     navigate("/");
     };
