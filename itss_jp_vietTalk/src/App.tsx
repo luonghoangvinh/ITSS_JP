dafd117cb1fee778501f55f5fdcd45841a7e0cfa
tunguyen260405 <tunguyen260405@gmail.com>
2026-05-12 11:04:52 +0700

    Fixing app.tsx

--- itss_jp_vietTalk/src/App.tsx
+++ itss_jp_vietTalk/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Mainlayout } from './layouts/Mainlayout'
 import { LogIn } from './pages/Login/LogIn'
+import { SignUp } from './pages/Signup/SignUp'
+import { LessonChoose } from './pages/LessonChoose/LessonChoose'
 
 function App() {
 
@@ -9,9 +11,12 @@ function App() {
     <div className="h-full w-full">
       <BrowserRouter>
         <Routes>
-          <Route index element={<LogIn />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/home" element={<Mainlayout />} />
+          <Route path="/" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Mainlayout />}>
+            <Route index element={<LessonChoose />} />
+            <Route path="lessons" element={<LessonChoose />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
