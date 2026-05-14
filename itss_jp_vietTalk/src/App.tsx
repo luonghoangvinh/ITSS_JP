a8bfd3e00a29e12ca7abcb367bb5aa74ad83689f
luonghoangvinh <lhvinh2003@gmail.com>
2026-05-14 21:58:53 +0700

    thêm logic cho màn chọn level và sửa các chỉ tiết để giống tài liệu đặc tả

--- itss_jp_vietTalk/src/App.tsx
+++ itss_jp_vietTalk/src/App.tsx
@@ -4,6 +4,7 @@ import { Mainlayout } from './layouts/Mainlayout'
 import { Login } from './pages/Login/Login'
 import { SignUp } from './pages/Signup/SignUp'
 import { LessonChoose } from './pages/LessonChoose/LessonChoose'
+import LevelSelectPage from './pages/LevelSelectPage/LevelSelectPage'
 
 function App() {
 
@@ -16,7 +17,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
 
           <Route path="/home" element={<Mainlayout />}>
-            <Route index element={<LessonChoose />} />
+            <Route index element={<LevelSelectPage />} />
             <Route path="lessons" element={<LessonChoose />} />
           </Route>
         </Routes>
---
+++ itss_jp_vietTalk/src/types/LevelSelectType.ts
@@ -0,0 +1,12 @@
+export default interface LevelSelectType {
+    id: string,
+    tag: string,
+    tagColor: any,
+    headerBg: string,
+    emoji: any,
+    progress: number,
+    progressColor: string,
+    name: string,
+    desc: string,
+    unlocked: boolean,
+}
