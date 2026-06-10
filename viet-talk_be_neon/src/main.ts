4e21e62c067a19b0519dee241146247eab76615a
luonghoangvinh <lhvinh2003@gmail.com>
2026-06-10 17:48:27 +0700

    fix lỗi thừa '/' tại maint.ts

--- viet-talk_be_neon/src/main.ts
+++ viet-talk_be_neon/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://itss-jp.netlify.app/'
+      'https://itss-jp.netlify.app'
     ],
     credentials: true,
   });
