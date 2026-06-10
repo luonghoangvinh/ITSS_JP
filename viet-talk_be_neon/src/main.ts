2ccd202236761e8c42d33080bc3969d946cc6434
luonghoangvinh <lhvinh2003@gmail.com>
2026-06-10 16:26:07 +0700

    sửa main.ts để fix lỗi cors

--- viet-talk_be_neon/src/main.ts
+++ viet-talk_be_neon/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.enableCors({
     origin: [
       'http://localhost:5173',
@@ -11,5 +10,7 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+  await app.listen(process.env.PORT ?? 3000);
+  
 }
 bootstrap();
